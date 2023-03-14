74089a5daacaaa5bb98bd13bde0f48a6e113ffe0
Thadigotla <Sreekanthsreekanth970@gmail.com>
2023-03-14 14:59:14 +0530

    my commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,26 @@
 import '../styles/globals.css';
 import { Toaster } from 'react-hot-toast';
 import { UserProvider } from '../UserProvider';
+import { NhostProvider, NhostClient } from '@nhost/nextjs';
+
+
+const nhost = new NhostClient({
+  subdomain: process.env.NEXT_PUBLIC_NHOST_SUBDOMAIN || 'ctzrqiexgysncscklyqy',
+  region: process.env.NEXT_PUBLIC_NHOST_REGION || 'eu-west-2'
+});
+
+
+
 
 function MyApp({ Component, pageProps }) {
   return (
+  <NhostProvider nhost={nhost} initial={pageProps.nhostSession}>
     <UserProvider>
       <Component {...pageProps} />
       <Toaster />
     </UserProvider>
+   </NhostProvider>
+
   );
 }
 
